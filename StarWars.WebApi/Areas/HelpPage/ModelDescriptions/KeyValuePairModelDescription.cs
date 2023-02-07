7d92666d993b6ad63fb2ab729004905034006462
Chet Gray <chet.gray@waystar.com>
2023-02-07 13:48:24 -0500

    Add project files.

---
+++ StarWars.WebApi/Areas/HelpPage/ModelDescriptions/KeyValuePairModelDescription.cs
@@ -0,0 +1,9 @@
+namespace StarWars.WebApi.Areas.HelpPage.ModelDescriptions
+{
+    public class KeyValuePairModelDescription : ModelDescription
+    {
+        public ModelDescription KeyModelDescription { get; set; }
+
+        public ModelDescription ValueModelDescription { get; set; }
+    }
+}
---
+++ StarWars.WebApi/Global.asax
@@ -0,0 +1,1 @@
+﻿<%@ Application Codebehind="Global.asax.cs" Inherits="StarWars.WebApi.WebApiApplication" Language="C#" %>
