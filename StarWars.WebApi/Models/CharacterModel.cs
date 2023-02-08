976d3527d631073db40fb89940cefdfd3bd6d475
Chet Gray <chet.gray@waystar.com>
2023-02-08 15:03:27 -0500

    🐛 fix: de/serialize allegiance and trilogy from/to strings

--- StarWars.WebApi/Models/CharacterModel.cs
+++ StarWars.WebApi/Models/CharacterModel.cs
@@ -1,17 +1,38 @@
-﻿namespace StarWars.WebApi.Models
+﻿using System.Runtime.Serialization;
+
+using Newtonsoft.Json;
+using Newtonsoft.Json.Converters;
+
+namespace StarWars.WebApi.Models
 {
+    // Serialize to string value
+    [JsonConverter(typeof(StringEnumConverter))]
     public enum Allegiance
     {
+        [EnumMember(Value = "None")]
         None = 0,
+
+        [EnumMember(Value = "Rebellion")]
         Rebellion,
+
+        [EnumMember(Value = "Empire")]
         Empire,
     }
 
+    // Serialize to string value
+    [JsonConverter(typeof(StringEnumConverter))]
     public enum Trilogy
     {
+        [EnumMember(Value = "None")]
         None = 0,
+
+        [EnumMember(Value = "Original")]
         Original,
+
+        [EnumMember(Value = "Prequel")]
         Prequel,
+
+        [EnumMember(Value = "Sequel")]
         Sequel,
     }
 
